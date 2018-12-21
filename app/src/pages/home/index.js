1d95a76ecd646a48950116a36c25f32c49f6d8b1
zhangbc <123456>
2018-12-21 17:26:25 +0800

    init

---
+++ app/src/pages/home/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './style.scss';
+
+export default class extends Component {
+  render() {
+    return (
+      <div>
+      往
+      </div>
+    );
+  }
+}
+
