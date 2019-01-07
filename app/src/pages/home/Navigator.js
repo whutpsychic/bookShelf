4c24849cd5f877e18ec77128e878e785ea90a520
zhangbc <123456>
2019-01-07 10:12:13 +0800

    。。

--- app/src/pages/home/Navigator.js
+++ app/src/pages/home/Navigator.js
@@ -1,22 +1,81 @@
 import React from "react";
 
 import { Affix, Menu, Icon, Divider, Input } from "antd";
-
 import logo from "./img/antd-logo.svg";
 
 const SubMenu = Menu.SubMenu;
 const MenuItemGroup = Menu.ItemGroup;
 const Search = Input.Search;
 
+// <Menu.Item key="mail">
+// 									<Icon type="mail" />
+// 									Navigation One
+// 								</Menu.Item>
+// 								<Menu.Item key="app" disabled>
+// 									<Icon type="appstore" />
+// 									Navigation Two
+// 								</Menu.Item>
+// 								<SubMenu
+// 									title={
+// 										<span className="submenu-title-wrapper">
+// 											<Icon type="cloud" />
+// 											名作大赏
+// 										</span>
+// 									}
+// 								>
+// 									<MenuItemGroup>
+// 										<Menu.Item key="setting:1">Option 1</Menu.Item>
+// 										<Menu.Item key="setting:2">Option 2</Menu.Item>
+// 										<Menu.Item key="setting:3">Option 3</Menu.Item>
+// 										<Menu.Item key="setting:4">Option 4</Menu.Item>
+// 									</MenuItemGroup>
+// 								</SubMenu>
+// 								<SubMenu
+// 									title={
+// 										<span className="submenu-title-wrapper">
+// 											<Icon type="cloud" />
+// 											开源项目快查
+// 										</span>
+// 									}
+// 								>
+// 									<MenuItemGroup>
+// 										<Menu.Item key="setting:1">Option 1</Menu.Item>
+// 										<Menu.Item key="setting:2">Option 2</Menu.Item>
+// 										<Menu.Item key="setting:3">Option 3</Menu.Item>
+// 										<Menu.Item key="setting:4">Option 4</Menu.Item>
+// 									</MenuItemGroup>
+// 								</SubMenu>
+// 								<Menu.Item key="alipay">
+// 									<a
+// 										href="https://ant.design"
+// 										target="_blank"
+// 										rel="noopener noreferrer"
+// 									>
+// 										Navigation Four - Link
+// 									</a>
+// 								</Menu.Item>
+
 export default class extends React.Component {
 	state = {
 		top: 0,
-		current: "mail"
+		current: "mail",
+		db: {}
 	};
 
-	componentDidMount() {}
+	componentDidMount() {
+		import("./db.js").then(db => {
+			this.setState({
+				db: db.default
+			});
+		});
+	}
 
 	render() {
+		console.log(db);
+		const { db } = this.state;
+
+		const { topMenu = [], topMenuIcons=[] } = db;
+
 		return (
 			<div>
 				<header className="home-header" />
@@ -42,40 +101,21 @@ export default class extends React.Component {
 								selectedKeys={[this.state.current]}
 								mode="horizontal"
 							>
-								<Menu.Item key="mail">
-									<Icon type="mail" />
-									Navigation One
-								</Menu.Item>
-								<Menu.Item key="app" disabled>
-									<Icon type="appstore" />
-									Navigation Two
-								</Menu.Item>
-								<SubMenu
-									title={
-										<span className="submenu-title-wrapper">
-											<Icon type="setting" />
-											Navigation Three - Submenu
-										</span>
+								{topMenu.map((item, i) => {
+									if (!item.children)
+										return <Menu.Item key={item.title}><Icon type={topMenuIcons[i]}/>{item.title}</Menu.Item>;
+									else {
+										return (
+											<SubMenu key={item.title} title={<span><Icon type={topMenuIcons[i]}/>{item.title}</span>}>
+												<MenuItemGroup>
+													{item.children.map((_item, _i) => {
+														return <Menu.Item key={_item.title}>{_item.title}</Menu.Item>;
+													})}
+												</MenuItemGroup>
+											</SubMenu>
+										);
 									}
-								>
-									<MenuItemGroup title="Item 1">
-										<Menu.Item key="setting:1">Option 1</Menu.Item>
-										<Menu.Item key="setting:2">Option 2</Menu.Item>
-									</MenuItemGroup>
-									<MenuItemGroup title="Item 2">
-										<Menu.Item key="setting:3">Option 3</Menu.Item>
-										<Menu.Item key="setting:4">Option 4</Menu.Item>
-									</MenuItemGroup>
-								</SubMenu>
-								<Menu.Item key="alipay">
-									<a
-										href="https://ant.design"
-										target="_blank"
-										rel="noopener noreferrer"
-									>
-										Navigation Four - Link
-									</a>
-								</Menu.Item>
+								})}
 							</Menu>
 						</div>
 					</div>
---
+++ app/src/pages/home/db.js
@@ -0,0 +1,16 @@
+const Home_Page = {
+	topMenu: [
+		{ title: "目录1" },
+		{
+			title: "名作大赏",
+			children: [{ title: "op1" }, { title: "op2" }, { title: "op3" }]
+		},
+		{
+			title: "开源快查",
+			children: [{ title: "op1" }, { title: "op2" }, { title: "op3" }]
+		}
+	],
+	topMenuIcons:["book","cloud","cloud"]
+};
+
+export default Home_Page;
